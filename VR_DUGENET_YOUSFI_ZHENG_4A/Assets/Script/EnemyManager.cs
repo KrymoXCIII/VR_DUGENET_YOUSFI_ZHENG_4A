deb92c383f9392e5816cddfbbf87a7666be80d8c
Denis ZHENG <dzheng2@myges.fr>
2023-01-06 15:00:59 +0100

    RESET 2.0

---
+++ VR_DUGENET_YOUSFI_ZHENG_4A/Assets/Script/EnemyManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int PV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
